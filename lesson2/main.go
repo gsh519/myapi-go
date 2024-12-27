6801c49d5ac13415faee34ad136863f3f7cdac4e
gsh519 <gsh05196yu@yahoo.co.jp>
2024-12-27 14:10:07 +0900

    2-2 構造体をjson化する

--- lesson2/main.go
+++ lesson2/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"time"
 )
@@ -47,5 +48,12 @@ func main() {
 		CreatedAt:   time.Now(),
 	}
 
-	fmt.Printf("%+v\n", article)
+	jsonData, err := json.Marshal(article)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("%s\n", jsonData)
 }
