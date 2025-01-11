fca277fe7ed25f4dec7c5c1c5a73cfa574dc8f12
gsh519 <gsh05196yu@yahoo.co.jp>
2025-01-11 11:37:32 +0900

    11-2 トレースIDの作成

--- api/middlewares/logging.go
+++ api/middlewares/logging.go
@@ -7,9 +7,14 @@ import (
 
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		traceID := newTraceID()
+
 		// リクエスト情報をロギング
-		log.Println(req.RequestURI, req.Method)
+		log.Printf("[%d]%s %s\n", traceID, req.RequestURI, req.Method)
 
 		next.ServeHTTP(w, req)
+
+		// レスポンス情報をロギング
+		log.Printf("[%d]", traceID)
 	})
 }
