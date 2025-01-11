27bad63bee0108b45e1f48782014a9445bce4865
gsh519 <gsh05196yu@yahoo.co.jp>
2025-01-11 10:33:31 +0900

    middlewareの導入

--- api/router.go
+++ api/router.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/yourname/reponame/api/middlewares"
 	"github.com/yourname/reponame/controllers"
 	"github.com/yourname/reponame/services"
 )
@@ -22,6 +23,8 @@ func NewRouter(db *sql.DB) *mux.Router {
 	r.HandleFunc("/article/{id:[0-9]+}", aCon.ArticleDetailHandler).Methods(http.MethodGet)
 	r.HandleFunc("/article/nice", aCon.PostNiceHandler).Methods(http.MethodPost)
 	r.HandleFunc("/comment", cCon.PostCommentHandler).Methods(http.MethodPost)
+
+	r.Use(middlewares.LoggingMiddleware)
 
 	return r
 }
